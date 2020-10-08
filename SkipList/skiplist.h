fefa7fb5be9f880d1e0003d24e6b070aa8478227
Holworth <kangqihan17@mails.ucas.ac.cn>
2020-10-08 15:54:16 +0800

    add interface

--- SkipList/skiplist.h
+++ SkipList/skiplist.h
@@ -5,6 +5,29 @@
 #include <cstdlib>
 #include <stdint.h>
 
+class SkipList {
+
+    static const uint8_t kMaxLevel = 5;
+
+    struct SkipListNode {
+        int32_t data;
+        SkipListNode *next, *below;
+
+        SkipListNode(): data(0), next(nullptr), below(nullptr) {}
+    };
+
+    private:
+
+        SkipListNode list_[kMaxLevel + 1];
+
+    public:
+
+        bool Empty() const;
+        bool Insert(const int32_t data);
+        SkipListNode *Find(const int32_t data);
+        bool Delete(const int32_t data);
+};
+
 
 #endif
 
